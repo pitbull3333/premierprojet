22f01b8509cdcfba16d66fa239ba58e147b0233a
Xavier <xdupondpoubelle@example.com>
2025-10-23 16:57:37 +0200

    deploiment

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 import pkg from './package.json';
 
-import path from 'path'
-import { fileURLToPath } from 'url'
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+//import path from 'path'
+//import { fileURLToPath } from 'url'
+//const __filename = fileURLToPath(import.meta.url)
+//const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
