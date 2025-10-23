e343a8fd99c890e8650ef63a9965ac6cb67e38b1
Xavier <xdupondpoubelle@example.com>
2025-10-23 16:52:32 +0200

    deploiment

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ const __dirname = path.dirname(__filename)
 // https://vite.dev/config/
 export default defineConfig({
   server:{port:3000},
+  base: '/premierprojet/',
   plugins: [react()],
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
